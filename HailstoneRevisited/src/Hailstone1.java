5d5ef727ca6b5ca488418065740411fc9846df79
jakeavloard <37011490+CSn1prt@users.noreply.github.com>
2021-08-22 22:27:44 -0700

    HailstoneRevisited

---
+++ HailstoneRevisited/src/Hailstone1.java
@@ -0,0 +1,77 @@
+import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class Hailstone1 {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private Hailstone1() {
+    }
+
+    /**
+     * Put a short phrase describing the static method myMethod here.
+     */
+    private static void myMethod() {
+        /*
+         * Put your code for myMethod here
+         */
+    }
+
+    /**
+     * Generates and outputs the Hailstone series starting with the given
+     * {@code NaturalNumber}.
+     *
+     * @param n
+     *            the starting natural number
+     * @param out
+     *            the output stream
+     * @updates out.content
+     * @requires n > 0 and out.is_open
+     * @ensures out.content = #out.content * [the Hailstone series starting with
+     *          n]
+     */
+    private static void generateSeries(NaturalNumber n, SimpleWriter out) {
+        int x = n.toInt();
+
+        final int k = 3;
+        out.println(x);
+        while (x != 2) {
+            if (x % 2 == 0) {
+
+                x = x / 2;
+                out.println(x);
+            }
+            if (x % 2 == 1) {
+
+                x = k * x + 1;
+                out.println(x);
+            }
+
+        }
+        out.println("1");
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleWriter out = new SimpleWriter1L();
+        out.println("Hello World!2");
+        NaturalNumber template = new NaturalNumber2(84);
+        generateSeries(template, out);
+        out.close();
+    }
+
+}
